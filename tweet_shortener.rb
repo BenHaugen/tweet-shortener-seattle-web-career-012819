9a1eff5b6a40eb8979f2d9dab37c07ef2f1f8314
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-26 21:46:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  new_array = []
+  array = string.split(" ")
+  array.each do |word|
+    if dictionary.keys.include?(word.downcase)
+      new_array << dictionary[word]
+    else
+      new_array << word
+    end
+    end
+    new_array.join(" ")
+end 
